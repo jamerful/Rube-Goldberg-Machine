46d2ee00451f848fcfe66b215ea790974777bfdd
jamerful <50549055+jamerful@users.noreply.github.com>
2019-05-13 00:43:42 +0100

    Added menus

---
+++ Rube Goldberg Assignment 2/Assets/EndGameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Knife")) {
+            SceneManager.LoadScene(2);
+        }
+            }
+}
---
+++ Rube Goldberg Assignment 2/Assets/endScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endScript : MonoBehaviour
+{
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
