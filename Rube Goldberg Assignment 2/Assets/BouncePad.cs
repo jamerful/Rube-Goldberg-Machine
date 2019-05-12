260fec7d76561f7f271bf2b8af00e850c39654ab
jamerful <50549055+jamerful@users.noreply.github.com>
2019-05-12 17:21:21 +0100

    Added BouncePads

---
+++ Rube Goldberg Assignment 2/Assets/BouncePad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour{
+
+    public int Speed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            collision.gameObject.GetComponent<Rigidbody>
+                ().AddForce(Vector3.up * Speed);
+        }
+    }
+
+}
