546228bcb0ff23fe65846c0d003a10640d13f5a0
jamerful <50549055+jamerful@users.noreply.github.com>
2019-05-12 21:29:06 +0100

    Added paddle and particle

---
+++ Rube Goldberg Assignment 2/Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public GameObject myPaddle;
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+        myPaddle = GameObject.Find("RotPaddle");
+        rotationSpeed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = new Vector3(2, 0, 0);
+        myPaddle.transform.Rotate(rotation);
+	}
+}
---
+++ Rube Goldberg Assignment 2/Assets/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Transform teleportTarget;
+    public GameObject theBall;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        theBall.transform.position = teleportTarget.transform.position;
+    }
+
+
+}
+
+
